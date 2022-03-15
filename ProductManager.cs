5dfbae3277ce50e974e681d906148050c50db36b
Tunahan Berber <tunahanberber123@gmail.com>
2022-03-15 17:04:39 +0300

    Add project files.

---
+++ ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)  //Void emir gibi bir şeydir git getir der return etmene gerek yok. eğer metod sonucu devamında lazımsa void onu yapmaz return ile değeri alırsın ama void görevi yapar işini bititir.
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi.");
+        }
+    }
+}
